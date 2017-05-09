8ad9ae77ab58d3f1e53a489a2523147f05b15691
kukom6 <kukom6@hotmail.com>
2017-05-09 19:15:53 +0200

    Fix a few issues

--- src/main/java/cz/muni/fi/pa053/Service.java
+++ src/main/java/cz/muni/fi/pa053/Service.java
@@ -2,8 +2,9 @@ package cz.muni.fi.pa053;
  
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
@@ -20,35 +21,45 @@ import java.net.URL;
 public class Service {
 
 	private static final String WEATHER_API_KEY = "1a916200fcdebd243b41054ad54dc6c7";
+	private static final Logger LOG = LoggerFactory.getLogger(Service.class);
+
 
 	@GET
 	@Path("/service")
 	@Produces(MediaType.APPLICATION_XML)
-	public String getMsg(@QueryParam("query") String msg) throws Exception {
-		Number result = null;
-		if(msg.matches("[A-Z]{3}")){ //airport code with three letters
-			JSONObject airport = (JSONObject) new JSONParser()
-					.parse(getHTML("http://www.airport-data.com/api/ap_info.json?iata=" + msg));
-			System.out.println("Airport information is:\n" + airport.toJSONString());
-			if(airport.get("icao")!=null){ // if airport exist
-				result = getTemperatureOfAirport(airport);
+	public String getMsg(@QueryParam("query") String query) throws Exception {
+		LOG.info("The input param is: '" + query + "'");
+		Double result = null;
+		if(query.matches("[A-Z]{3}")){ //airport code with three letters
+			JSONObject airport = new JSONObject(getHTML("http://www.airport-data.com/api/ap_info.json?iata=" + query));
+			LOG.info("Airport information is:\n" + airport.toString(3));
+			if(airport.get("icao") != JSONObject.NULL){ // if airport exist
+				result = ((Number)getTemperatureOfAirport(airport)).doubleValue();
 			}else{
-				result = getValueOfStock(msg);
+				result = getValueOfStock(query);
 			}
-		}else if(msg.matches("[A-Z]{1,4}")){ //STOCK
-			result = getValueOfStock(msg);
-		}else if(msg.matches("([-+]?[0-9]*\\.?[0-9]+[\\/\\+\\-\\*])+([-+]?[0-9]*\\.?[0-9]+)")){ //math
-			result = executeExpression(msg);
+		}else if(query.matches("[A-Z]{1,4}")){ //STOCK
+			result = getValueOfStock(query);
+		}else{ //math expression otherwise
+			Expression e = new ExpressionBuilder(query)
+					.build();
+			if(e.validate().isValid()){ //if expression is valid
+				Double evaluated = e.evaluate();
+				LOG.info("The result of " + query + " is: " + evaluated);
+				result = evaluated;
+			}
 		}
 		if(result!=null){
-			return "<result>" + result.doubleValue() + "</result>";
+			LOG.info("Result for query " + query + " is: " + result);
+			return "<result>" + result + "</result>";
 		}else{
+			LOG.error("This query is not supported: " + query);
 			return "<result></result>";
 		}
 	}
 
 	private String getHTML(String urlToRead) throws Exception {
-		System.out.println("Get url is: " + urlToRead);
+		LOG.info("Get url is: " + urlToRead);
 		StringBuilder result = new StringBuilder();
 		URL url = new URL(urlToRead);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -62,28 +73,24 @@ public class Service {
 		return result.toString();
 	}
 
-	private Number getTemperatureOfAirport(JSONObject airportInfo) throws Exception {
+	private Object getTemperatureOfAirport(JSONObject airportInfo) throws Exception {
 		String longitude = (String) airportInfo.get("longitude");
 		String latitude = (String) airportInfo.get("latitude");
-		JSONObject weather = (JSONObject) new JSONParser()
-				.parse(getHTML("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude
+		JSONObject weather = new JSONObject(getHTML("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude
 						+ "&lon=" + longitude + "&appid=" + WEATHER_API_KEY
 						+ "&units=metric"));
-		System.out.println("Weather information is:\n" + weather.toJSONString());
-		return (Number)((JSONObject) weather.get("main")).get("temp");
+		LOG.info("Weather information is:\n" + weather.toString(3));
+		return ((JSONObject) weather.get("main")).get("temp");
 	}
 
-	private BigDecimal getValueOfStock(String stockSymbol) throws IOException {
+	private Double getValueOfStock(String stockSymbol) throws IOException {
+		Double result = null;
 		Stock stock = YahooFinance.get(stockSymbol);
-		System.out.println("Info about stock" + stock.toString());
-		return stock.getQuote().getPrice();
-	}
-
-	private double executeExpression(String expression){
-		Expression e = new ExpressionBuilder(expression)
-				.build();
-		Double evaluated = e.evaluate();
-		System.out.println("The result of " + expression + " is: " + evaluated);
-		return evaluated;
+		LOG.info("Info about stock" + stock.toString());
+		BigDecimal tempResult =  stock.getQuote().getPrice();
+		if(tempResult != null){
+			result = tempResult.doubleValue();
+		}
+		return result;
 	}
 }
